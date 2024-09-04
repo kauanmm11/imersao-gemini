7a67d86ac2360e892b0cecb1b22eff0b1a1c7937
kauanmm11 <kauanmm11@gmail.com>
2024-09-04 18:02:46 -0300

    criando os dados js

---
+++ dados.js
@@ -0,0 +1,26 @@
+let dados = [
+    {
+        titulo: "Alanzoka",
+        descricao: "Uma das maiores estrelas brasileiras da Twitch Alanzoka, nome artístico de Alan Ferreira Pereira, é um dos streamers brasileiros mais populares e influentes da plataforma Twitch. Conhecido por suas transmissões energéticas e divertidas, principalmente de games, Alanzoka conquistou uma legião de fãs que acompanham suas aventuras online. Sua espontaneidade e carisma o tornaram um dos principais nomes do cenário gamer brasileiro, contribuindo para o crescimento do streaming no país.",
+        links: [
+            "https://pt.wikipedia.org/wiki/Alanzoka",
+            "https://www.twitch.tv/alanzoka"
+        ]
+    },
+    {
+        titulo: "Yoda",
+        descricao: "Felipe 'YoDa' Noronha é um dos streamers brasileiros mais populares da Twitch, conhecido por suas transmissões de League of Legends e sua personalidade carismática. Ele conquistou uma grande base de fãs por sua habilidade no jogo e suas interações com a comunidade.",
+        links: [
+            "https://pt.wikipedia.org/wiki/YoDa",
+            "https://www.twitch.tv/yoda"
+        ]
+    },
+    {
+        titulo: "Rakin",
+        descricao: "Rafael 'Rakin' Knittel é outro grande nome do cenário de streaming brasileiro. Conhecido por suas transmissões divertidas e interativas de League of Legends, Rakin conquistou o público com seu humor e sua paixão por games. Ele é um dos principais streamers de League of Legends no Brasil.",
+        links: [
+            "https://ordemparanormal.fandom.com/wiki/Rakin",
+            "https://www.twitch.tv/rakin"
+        ]
+    }
+];
