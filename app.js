8fde9f13a83f30eda0e3ae0d4f6f3893fa34797b
kauanmm11 <kauanmm11@gmail.com>
2024-09-05 18:16:48 -0300

    javascript dados

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-console.log(dados)
+// console.log(dados)
+
+function pesquisar() {
+    let section = document.getElementById("resultados-pesquisa")
+    console.log(section);
+
+    let resultados = ""
+
+    for (let dado of dados) {
+        resultados += `
+        <div class="item-resultado">
+            <h2>${dado.titulo}</h2>
+        <div class="foto-texto">
+            <img src= ${dado.foto} alt="Imagem de Alanzoka" class="imagem-resultado">
+             <p class="descricao-meta">${dado.descricao}</p>
+        </div>
+            <a href= ${dado.links}>Mais Informações</a><br>
+            <a href= ${dado.links}>Alanzoka na Twitch</a>
+      </div>
+    `
+    section.innerHTML = resultados
+    }
+}
